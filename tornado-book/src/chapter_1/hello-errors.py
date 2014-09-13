67d1bf6a987dbd438d20a941f415044c807f31a9
Serge Catudal <serge.catudal@gmail.com>
2014-09-13 16:43:33 -0400

    Added the tornado book chapter 1 exercices

---
+++ tornado-book/src/chapter_1/hello-errors.py
@@ -0,0 +1,32 @@
+"""
+Example 1-3. Custom errors responses: hello-errors.py
+"""
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado.options import (
+    define,
+    options
+)
+
+define('port', default=8000, help='run on the given port', type=int)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', friendly user!')
+
+    def write_error(self, status_code, **kwargs):
+        self.write(
+            'Gosh darnit, user! You caused a {} error'.format(status_code))
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r'/', IndexHandler)])
+
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
